0ef924e74febd1cac2be07f53ac55eca911f845e
muhamadaji299 <majis1802@gmail.com>
2025-03-13 22:32:44 +0700

    crud api

--- routes/api.php
+++ routes/api.php
@@ -8,4 +8,10 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::apiResource('mahasiswa', MahasiswaController::class);
+Route::get('/mahasiswa', [MahasiswaController::class, 'index']);
+Route::post('/mahasiswa', [MahasiswaController::class, 'store']);
+Route::get('/mahasiswa/{id}', [MahasiswaController::class, 'show']);
+Route::put('/mahasiswa/{id}', [MahasiswaController::class, 'update']); // Gunakan ID
+Route::patch('/mahasiswa/{id}', [MahasiswaController::class, 'update']); // Gunakan ID
+Route::delete('/mahasiswa/{id}', [MahasiswaController::class, 'destroy']); // Gunakan ID
+
